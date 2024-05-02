3868f88c8eb7b7b0bd20361559ec481f7bc123a0
jorgecs <114354108+jorgecs@users.noreply.github.com>
2024-05-02 11:54:52 +0200

    Create testScheduler.py

---
+++ Test/testScheduler.py
@@ -0,0 +1,56 @@
+import requests
+import time
+import json 
+
+url = 'http://localhost:8082/'
+pathURL = 'url'
+pathResult = 'result'
+pathCircuit = 'circuit'
+
+ids = []
+
+i=0
+
+data = {"url":"https://algassert.com/quirk#circuit={'cols':[[1,1,'H'],[1,'X','•'],['X','•','•'],[1,1,'X'],[1,'X','•'],[1,'X'],['X','•','•'],[1,'X','X'],[1,'X','•'],['X','•','•'],[1,1,'X'],[1,'X','•'],[1,'X'],['X','•','•'],[1,'X','X'],['Measure','Measure','Measure']]}" ,"shots" : 1000, "provider":"ibm", "policy":"time"}
+ids.append((int(requests.post(url+pathURL, json = data).text.split("Your id is ")[1]), 10000))
+print(str(i)+" sent")
+i+=1
+
+data = {"url":"https://algassert.com/quirk#circuit={'cols':[[1,1,'H'],[1,'X','•'],['X','•','•'],[1,1,'X'],[1,'X','•'],[1,'X'],['X','•','•'],[1,'X','X'],[1,'X','•'],['X','•','•'],[1,1,'X'],[1,'X','•'],[1,'X'],['X','•','•'],[1,'X','X'],['Measure','Measure','Measure']]}", "shots" : 10000, "provider":"ibm", "policy":"time"}
+ids.append((int(requests.post(url+pathURL, json = data).text.split("Your id is ")[1]), 10000))
+print(str(i)+" sent")
+i+=1
+
+data = {"url":"https://raw.githubusercontent.com/jorgecs/CompositionCircuits/main/simon.py", "shots" : 10000, "policy":"time"}
+ids.append((int(requests.post(url+pathCircuit, json = data).text.split("Your id is ")[1]), 10000))
+print(str(i)+" sent")
+i+=1
+
+data = {"url":"https://algassert.com/quirk#circuit={'cols':[['H'],['•','X'], ['H','H',1],['Measure','Measure']]}", "shots" : 10000, "provider":"ibm", "policy":"shots"}
+ids.append((int(requests.post(url+pathURL, json = data).text.split("Your id is ")[1]), 10000))
+print(str(i)+" sent")
+i+=1
+
+data = {"url":"https://raw.githubusercontent.com/jorgecs/pythonscripts/main/shor7xMOD15Circuit.py", "shots" : 5000, "policy":"shots"}
+ids.append((int(requests.post(url+pathCircuit, json = data).text.split("Your id is ")[1]), 5000))
+print(str(i)+" sent")
+i+=1
+
+##############################################
+
+for j in range(30):
+    try:
+        policies = ["time", "depth", "shots", "shots_depth", "shots_optimized"]
+        import random
+        policy = 'depth'
+        #policy = policies[random.randint(0,4)]
+        data = {"url":"https://algassert.com/quirk#circuit={'cols':[[1,1,'H'],[1,'X','•'],['X','•','•'],[1,1,'X'],[1,'X','•'],[1,'X'],['X','•','•'],[1,'X','X'],[1,'X','•'],['X','•','•'],[1,1,'X'],[1,'X','•'],[1,'X'],['X','•','•'],[1,'X','X'],['Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure','Measure']]}", "shots" : 10000, "provider":"ibm", "policy":policy}
+        ids.append((int(requests.post(url+pathURL, json = data, timeout=5).text.split("Your id is ")[1]), 10000))
+        print(str(i)+" sent")
+        i+=1
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+    except Exception as e:
+        print("Error occurred: ", e)
+
+print(ids)
